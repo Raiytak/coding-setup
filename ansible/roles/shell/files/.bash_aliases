beac98debf8970466dc445ce29045cb32326aa30
Raiytak <mathieu.salaun12@gmail.com>
2024-01-04 23:17:03 -0500

    Add shell, ssh and git configurations

--- ansible/roles/shell/files/.bash_aliases
+++
@@ -1,2 +0,0 @@
-alias ssh-config="cat ~/.ssh/config"
-alias t="mkdir ~/tmp 2>/dev/null && cd ~/tmp"
--- scripts/launch_playbook.sh
+++ scripts/launch_playbook.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -eou pipefail
 
-user="${1}"
-hosts="${2}"
+user="$1"
+hosts="$2"
+verbose="${3:-}"
 
-ansible-playbook ansible/playbook.yml --extra-vars "USER=${user} HOSTS=${hosts}"
+ansible-playbook ansible/playbook.yml --extra-vars "USER=${user} HOSTS=${hosts}" ${verbose}
