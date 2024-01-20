9f626e4f0a279498d02232111d48aa092b658ec8
Raiytak <mathieu.salaun12@gmail.com>
2024-01-20 15:08:42 -0500

    Improve script and add default p10k configuration file

--- scripts/launch_playbook.sh
+++ scripts/launch_playbook.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -eou pipefail
 
-user="$1"
-hosts="$2"
+target_user="$1"
+target_host="$2"
 verbose="${3:-}"
 
-ansible-playbook ansible/playbook.yml --extra-vars "USER=${user} HOSTS=${hosts}" ${verbose}
+ansible-playbook ansible/playbook.yml --extra-vars "TARGET_USER=${target_user} TARGET_HOST=${target_host}" ${verbose}
